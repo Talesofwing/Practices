7edb9fb6e6f072bf5c4c25d645ac0aeedef51a60
zer0 <zerox0103@gmail.com>
2025-09-21 23:26:03 +0900

    [GAMES101] Finished `Homework_06`

--- 30.ComputerGrpahics/003.01.GAMES101/Homework_06/BVH.cpp
+++ 30.ComputerGrpahics/003.01.GAMES101/Homework_06/BVH.cpp
@@ -1,109 +1,175 @@
 #include <algorithm>
 #include <cassert>
+#include <chrono>
+
 #include "BVH.hpp"
 
+using namespace std::chrono;
+
+#define SPLIT_METHOD 1	// 0 : BVH, 1 : SAH
+
 BVHAccel::BVHAccel(std::vector<Object*> p, int maxPrimsInNode,
-                   SplitMethod splitMethod)
-    : maxPrimsInNode(std::min(255, maxPrimsInNode)), splitMethod(splitMethod),
-      primitives(std::move(p))
-{
-    time_t start, stop;
-    time(&start);
-    if (primitives.empty())
-        return;
+				   SplitMethod splitMethod)
+	: maxPrimsInNode(std::min(255, maxPrimsInNode)), splitMethod(splitMethod),
+	primitives(std::move(p)) {
+	time_t start, stop;
+	time(&start);
+	if (primitives.empty())
+		return;
 
-    root = recursiveBuild(primitives);
+	std::cout << " --- Building BVH ---" << std::endl;
+	auto s = high_resolution_clock::now();
+	root = recursiveBuild(primitives);
+	auto e = high_resolution_clock::now();
+	auto d = duration_cast<milliseconds>(e - s);
 
-    time(&stop);
-    double diff = difftime(stop, start);
-    int hrs = (int)diff / 3600;
-    int mins = ((int)diff / 60) - (hrs * 60);
-    int secs = (int)diff - (hrs * 3600) - (mins * 60);
+	std::cout << "BVH took " << d.count() / 1000.0 << "s, " << d.count() << "ms" << std::endl;
 
-    printf(
-        "\rBVH Generation complete: \nTime Taken: %i hrs, %i mins, %i secs\n\n",
-        hrs, mins, secs);
+	time(&stop);
+	double diff = difftime(stop, start);
+	int hrs = (int)diff / 3600;
+	int mins = ((int)diff / 60) - (hrs * 60);
+	int secs = (int)diff - (hrs * 3600) - (mins * 60);
+
+	printf(
+		"\rBVH Generation complete: \nTime Taken: %i hrs, %i mins, %i secs\n\n",
+		hrs, mins, secs);
 }
 
-BVHBuildNode* BVHAccel::recursiveBuild(std::vector<Object*> objects)
-{
-    BVHBuildNode* node = new BVHBuildNode();
+BVHBuildNode* BVHAccel::recursiveBuild(std::vector<Object*> objects) {
+	BVHBuildNode* node = new BVHBuildNode();
+	std::vector<Object*>::iterator middling;
 
-    // Compute bounds of all primitives in BVH node
-    Bounds3 bounds;
-    for (int i = 0; i < objects.size(); ++i)
-        bounds = Union(bounds, objects[i]->getBounds());
-    if (objects.size() == 1) {
-        // Create leaf _BVHBuildNode_
-        node->bounds = objects[0]->getBounds();
-        node->object = objects[0];
-        node->left = nullptr;
-        node->right = nullptr;
-        return node;
-    }
-    else if (objects.size() == 2) {
-        node->left = recursiveBuild(std::vector{objects[0]});
-        node->right = recursiveBuild(std::vector{objects[1]});
+	// Compute bounds of all primitives in BVH node
+	Bounds3 bounds;
+	for (int i = 0; i < objects.size(); ++i)
+		bounds = Union(bounds, objects[i]->getBounds());
 
-        node->bounds = Union(node->left->bounds, node->right->bounds);
-        return node;
-    }
-    else {
-        Bounds3 centroidBounds;
-        for (int i = 0; i < objects.size(); ++i)
-            centroidBounds =
-                Union(centroidBounds, objects[i]->getBounds().Centroid());
-        int dim = centroidBounds.maxExtent();
-        switch (dim) {
-        case 0:
-            std::sort(objects.begin(), objects.end(), [](auto f1, auto f2) {
-                return f1->getBounds().Centroid().x <
-                       f2->getBounds().Centroid().x;
-            });
-            break;
-        case 1:
-            std::sort(objects.begin(), objects.end(), [](auto f1, auto f2) {
-                return f1->getBounds().Centroid().y <
-                       f2->getBounds().Centroid().y;
-            });
-            break;
-        case 2:
-            std::sort(objects.begin(), objects.end(), [](auto f1, auto f2) {
-                return f1->getBounds().Centroid().z <
-                       f2->getBounds().Centroid().z;
-            });
-            break;
-        }
+	if (objects.size() == 1) {
+		// Create leaf _BVHBuildNode_
+		node->bounds = objects[0]->getBounds();
+		node->object = objects[0];
+		node->left = nullptr;
+		node->right = nullptr;
+		return node;
+	} else if (objects.size() == 2) {
+		node->left = recursiveBuild(std::vector {objects[0]});
+		node->right = recursiveBuild(std::vector {objects[1]});
 
-        auto beginning = objects.begin();
-        auto middling = objects.begin() + (objects.size() / 2);
-        auto ending = objects.end();
+		node->bounds = Union(node->left->bounds, node->right->bounds);
+		return node;
+	} else {
+		Bounds3 centroidBounds;
+		for (int i = 0; i < objects.size(); ++i)
+			centroidBounds = Union(centroidBounds, objects[i]->getBounds().Centroid());
 
-        auto leftshapes = std::vector<Object*>(beginning, middling);
-        auto rightshapes = std::vector<Object*>(middling, ending);
+		int dim = centroidBounds.maxExtent();
+		switch (dim) {
+			case 0:
+				std::sort(objects.begin(), objects.end(), [](auto f1, auto f2) {
+					return f1->getBounds().Centroid().x <
+						f2->getBounds().Centroid().x;
+				});
+				break;
+			case 1:
+				std::sort(objects.begin(), objects.end(), [](auto f1, auto f2) {
+					return f1->getBounds().Centroid().y <
+						f2->getBounds().Centroid().y;
+				});
+				break;
+			case 2:
+				std::sort(objects.begin(), objects.end(), [](auto f1, auto f2) {
+					return f1->getBounds().Centroid().z <
+						f2->getBounds().Centroid().z;
+				});
+				break;
+		}
 
-        assert(objects.size() == (leftshapes.size() + rightshapes.size()));
+		switch (SPLIT_METHOD) {
+			case 0:
+				middling = objects.begin() + (objects.size() / 2);
+				break;
+			case 1:
+				float maxArea = centroidBounds.SurfaceArea();
+				float minCost = std::numeric_limits<float>::infinity();
+				int minClipNum = 0;
+				float BoundingClip = 2;		// 分成多分份 (去掉就是真正的SAH)
 
-        node->left = recursiveBuild(leftshapes);
-        node->right = recursiveBuild(rightshapes);
+				float BoundingClipDiv = 1.0f / BoundingClip;
+				float maxAreaDiv = 1.0f / maxArea;
+				for (int i = 0; i < BoundingClip; i++) {
+					Bounds3 leftBounds3;
+					Bounds3 rightBounds3;
+					float leftCount = 0.0f;
+					float rightCount = 0.0f;
+					auto middlingTemp = objects.begin() + std::floor(objects.size() * (i * BoundingClipDiv));
+					for (auto j = objects.begin(); j < middlingTemp; j++) {
+						leftBounds3 = Union(leftBounds3, (*j)->getBounds().Centroid());
+						leftCount = leftCount + 1.0f;
+					}
+					for (auto j = middlingTemp; j < objects.end(); j++) {
+						rightBounds3 = Union(rightBounds3, (*j)->getBounds().Centroid());
+						rightCount = rightCount + 1.0f;
+					}
 
-        node->bounds = Union(node->left->bounds, node->right->bounds);
-    }
+					// CI和CT都分別除了8
+					float cost = 0.125f + (leftCount * leftBounds3.SurfaceArea() + rightCount * rightBounds3.SurfaceArea()) * maxAreaDiv;
+					if (cost < minCost) {
+						minClipNum = i;
+						minCost = cost;
+					}
+				}
+				middling = objects.begin() + std::floor(objects.size() * (minClipNum * BoundingClipDiv));
+				break;
+		}
 
-    return node;
+		auto beginning = objects.begin();
+		auto ending = objects.end();
+
+		auto leftshapes = std::vector<Object*>(beginning, middling);
+		auto rightshapes = std::vector<Object*>(middling, ending);
+
+		assert(objects.size() == (leftshapes.size() + rightshapes.size()));
+
+		node->left = recursiveBuild(leftshapes);
+		node->right = recursiveBuild(rightshapes);
+
+		node->bounds = Union(node->left->bounds, node->right->bounds);
+	}
+
+	return node;
 }
 
-Intersection BVHAccel::Intersect(const Ray& ray) const
-{
-    Intersection isect;
-    if (!root)
-        return isect;
-    isect = BVHAccel::getIntersection(root, ray);
-    return isect;
+Intersection BVHAccel::Intersect(const Ray& ray) const {
+	Intersection isect;
+	if (!root)
+		return isect;
+	isect = BVHAccel::getIntersection(root, ray);
+	return isect;
 }
 
-Intersection BVHAccel::getIntersection(BVHBuildNode* node, const Ray& ray) const
-{
-    // TODO Traverse the BVH to find intersection
+Intersection BVHAccel::getIntersection(BVHBuildNode* node, const Ray& ray) const {
+	// TODO Traverse the BVH to find intersection
+	Intersection inter;
 
+	Vector3f invdir(1.0f / ray.direction.x, 1.0f / ray.direction.y, 1.0f / ray.direction.z);
+	std::array<int, 3> dirIsNeg;
+	dirIsNeg[0] = invdir.x < 0;
+	dirIsNeg[1] = invdir.y < 0;
+	dirIsNeg[2] = invdir.z < 0;
+
+	if (node->bounds.IntersectP(ray, ray.direction_inv, dirIsNeg)) {
+		if (!node->right && !node->left) {
+			return node->object->getIntersection(ray);
+		}
+
+		Intersection leftInter = getIntersection(node->left, ray);
+		Intersection rightInter = getIntersection(node->right, ray);
+		if (leftInter.distance < rightInter.distance)
+			inter = leftInter;
+		else
+			inter = rightInter;
+	}
+
+	return inter;
 }
--- 30.ComputerGrpahics/003.01.GAMES101/Homework_06/Renderer.cpp
+++ 30.ComputerGrpahics/003.01.GAMES101/Homework_06/Renderer.cpp
@@ -14,42 +14,43 @@ const float EPSILON = 0.00001;
 // The main render function. This where we iterate over all pixels in the image,
 // generate primary rays and cast these rays into the scene. The content of the
 // framebuffer is saved to a file.
-void Renderer::Render(const Scene& scene)
-{
-    std::vector<Vector3f> framebuffer(scene.width * scene.height);
+void Renderer::Render(const Scene& scene) {
+	std::vector<Vector3f> framebuffer(scene.width * scene.height);
 
-    float scale = tan(deg2rad(scene.fov * 0.5));
-    float imageAspectRatio = scene.width / (float)scene.height;
-    Vector3f eye_pos(-1, 5, 10);
-    int m = 0;
-    for (uint32_t j = 0; j < scene.height; ++j) {
-        for (uint32_t i = 0; i < scene.width; ++i) {
-            // generate primary ray direction
-            float x = (2 * (i + 0.5) / (float)scene.width - 1) *
-                      imageAspectRatio * scale;
-            float y = (1 - 2 * (j + 0.5) / (float)scene.height) * scale;
-            // TODO: Find the x and y positions of the current pixel to get the
-            // direction
-            //  vector that passes through it.
-            // Also, don't forget to multiply both of them with the variable
-            // *scale*, and x (horizontal) variable with the *imageAspectRatio*
+	float scale = tan(deg2rad(scene.fov * 0.5));
+	float imageAspectRatio = scene.width / (float)scene.height;
+	Vector3f eye_pos(-1, 5, 10);
+	int m = 0;
+	for (uint32_t j = 0; j < scene.height; ++j) {
+		for (uint32_t i = 0; i < scene.width; ++i) {
+			// generate primary ray direction
 
-            // Don't forget to normalize this direction!
+			// TODO: Find the x and y positions of the current pixel to get the
+			// direction
+			//  vector that passes through it.
+			// Also, don't forget to multiply both of them with the variable
+			// *scale*, and x (horizontal) variable with the *imageAspectRatio*
 
-        }
-        UpdateProgress(j / (float)scene.height);
-    }
-    UpdateProgress(1.f);
+			float y = (1.0f - 2.0f * (j + 0.5f) / static_cast<float>(scene.height - 1)) * scale;
+			float x = (2.0f * (i + 0.5f) / static_cast<float>(scene.width - 1) - 1.0f) * imageAspectRatio * scale;
 
-    // save framebuffer to file
-    FILE* fp = fopen("binary.ppm", "wb");
-    (void)fprintf(fp, "P6\n%d %d\n255\n", scene.width, scene.height);
-    for (auto i = 0; i < scene.height * scene.width; ++i) {
-        static unsigned char color[3];
-        color[0] = (unsigned char)(255 * clamp(0, 1, framebuffer[i].x));
-        color[1] = (unsigned char)(255 * clamp(0, 1, framebuffer[i].y));
-        color[2] = (unsigned char)(255 * clamp(0, 1, framebuffer[i].z));
-        fwrite(color, 1, 3, fp);
-    }
-    fclose(fp);    
+			Vector3f dir = normalize(Vector3f(x, y, -1)); // Don't forget to normalize this direction!
+			Ray ray(eye_pos, dir);
+			framebuffer[m++] = scene.castRay(ray, 0);
+		}
+		UpdateProgress(j / (float)scene.height);
+	}
+	UpdateProgress(1.f);
+
+	// save framebuffer to file
+	FILE* fp = fopen("binary.ppm", "wb");
+	(void)fprintf(fp, "P6\n%d %d\n255\n", scene.width, scene.height);
+	for (auto i = 0; i < scene.height * scene.width; ++i) {
+		static unsigned char color[3];
+		color[0] = (unsigned char)(255 * clamp(0, 1, framebuffer[i].x));
+		color[1] = (unsigned char)(255 * clamp(0, 1, framebuffer[i].y));
+		color[2] = (unsigned char)(255 * clamp(0, 1, framebuffer[i].z));
+		fwrite(color, 1, 3, fp);
+	}
+	fclose(fp);
 }
