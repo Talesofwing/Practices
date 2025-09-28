69aaec174824daad1990abafb42d826c957641e8
zer0 <zerox0103@gmail.com>
2025-09-28 14:47:20 +0900

    [GAMES101] Finished `Homework_07`

--- 30.ComputerGrpahics/003.01.GAMES101/Homework_07/Renderer.cpp
+++ 30.ComputerGrpahics/003.01.GAMES101/Homework_07/Renderer.cpp
@@ -6,6 +6,8 @@
 #include "Scene.hpp"
 #include "Renderer.hpp"
 
+#include <thread>
+#include <omp.h>
 
 inline float deg2rad(const float& deg) { return deg * M_PI / 180.0; }
 
@@ -14,44 +16,48 @@ const float EPSILON = 0.00001;
 // The main render function. This where we iterate over all pixels in the image,
 // generate primary rays and cast these rays into the scene. The content of the
 // framebuffer is saved to a file.
-void Renderer::Render(const Scene& scene)
-{
-    std::vector<Vector3f> framebuffer(scene.width * scene.height);
+void Renderer::Render(const Scene& scene) {
+	std::vector<Vector3f> framebuffer(scene.width * scene.height);
 
-    float scale = tan(deg2rad(scene.fov * 0.5));
-    float imageAspectRatio = scene.width / (float)scene.height;
-    Vector3f eye_pos(278, 273, -800);
-    int m = 0;
+	float scale = tan(deg2rad(scene.fov * 0.5));
+	float imageAspectRatio = scene.width / (float)scene.height;
+	Vector3f eye_pos(278, 273, -800);
 
-    // change the spp value to change sample ammount
-    int spp = 16;
-    std::cout << "SPP: " << spp << "\n";
-    for (uint32_t j = 0; j < scene.height; ++j) {
-        for (uint32_t i = 0; i < scene.width; ++i) {
-            // generate primary ray direction
-            float x = (2 * (i + 0.5) / (float)scene.width - 1) *
-                      imageAspectRatio * scale;
-            float y = (1 - 2 * (j + 0.5) / (float)scene.height) * scale;
+	int spp = 128;
+	std::cout << "SPP: " << spp << "\n";
+	uint32_t hxw = scene.height * scene.width;
+	std::atomic<int> process {0};
+	int step = hxw / 100;
+#pragma omp parallel for
+	for (uint32_t p = 0; p < hxw; ++p) {
+		int i = p % scene.height;
+		int j = p / scene.height;
 
-            Vector3f dir = normalize(Vector3f(-x, y, 1));
-            for (int k = 0; k < spp; k++){
-                framebuffer[m] += scene.castRay(Ray(eye_pos, dir), 0) / spp;  
-            }
-            m++;
-        }
-        UpdateProgress(j / (float)scene.height);
-    }
-    UpdateProgress(1.f);
+		float x = (2 * (i + 0.5f) / scene.width - 1) * imageAspectRatio * scale;
+		float y = (1 - 2 * (j + 0.5f) / scene.height) * scale;
 
-    // save framebuffer to file
-    FILE* fp = fopen("binary.ppm", "wb");
-    (void)fprintf(fp, "P6\n%d %d\n255\n", scene.width, scene.height);
-    for (auto i = 0; i < scene.height * scene.width; ++i) {
-        static unsigned char color[3];
-        color[0] = (unsigned char)(255 * std::pow(clamp(0, 1, framebuffer[i].x), 0.6f));
-        color[1] = (unsigned char)(255 * std::pow(clamp(0, 1, framebuffer[i].y), 0.6f));
-        color[2] = (unsigned char)(255 * std::pow(clamp(0, 1, framebuffer[i].z), 0.6f));
-        fwrite(color, 1, 3, fp);
-    }
-    fclose(fp);    
+		Vector3f dir = normalize(Vector3f(-x, y, 1));
+		for (int k = 0; k < spp; k++) {
+			framebuffer[p] += scene.castRay(Ray(eye_pos, dir), 0) / spp;
+		}
+
+		int done = ++process;
+		if (done % step == 0 || done == hxw) {
+#pragma omp critical
+			UpdateProgress(done / (float)hxw);
+		}
+	}
+	UpdateProgress(1.f);
+
+	// save framebuffer to file
+	FILE* fp = fopen("binary.ppm", "wb");
+	(void)fprintf(fp, "P6\n%d %d\n255\n", scene.width, scene.height);
+	for (auto i = 0; i < scene.height * scene.width; ++i) {
+		static unsigned char color[3];
+		color[0] = (unsigned char)(255 * std::pow(clamp(0, 1, framebuffer[i].x), 0.6f));
+		color[1] = (unsigned char)(255 * std::pow(clamp(0, 1, framebuffer[i].y), 0.6f));
+		color[2] = (unsigned char)(255 * std::pow(clamp(0, 1, framebuffer[i].z), 0.6f));
+		fwrite(color, 1, 3, fp);
+	}
+	fclose(fp);
 }
--- 30.ComputerGrpahics/003.01.GAMES101/Homework_07/Scene.cpp
+++ 30.ComputerGrpahics/003.01.GAMES101/Homework_07/Scene.cpp
@@ -6,59 +6,109 @@
 
 
 void Scene::buildBVH() {
-    printf(" - Generating BVH...\n\n");
-    this->bvh = new BVHAccel(objects, 1, BVHAccel::SplitMethod::NAIVE);
+	printf(" - Generating BVH...\n\n");
+	this->bvh = new BVHAccel(objects, 1, BVHAccel::SplitMethod::NAIVE);
 }
 
-Intersection Scene::intersect(const Ray &ray) const
-{
-    return this->bvh->Intersect(ray);
+Intersection Scene::intersect(const Ray& ray) const {
+	return this->bvh->Intersect(ray);
 }
 
-void Scene::sampleLight(Intersection &pos, float &pdf) const
-{
-    float emit_area_sum = 0;
-    for (uint32_t k = 0; k < objects.size(); ++k) {
-        if (objects[k]->hasEmit()){
-            emit_area_sum += objects[k]->getArea();
-        }
-    }
-    float p = get_random_float() * emit_area_sum;
-    emit_area_sum = 0;
-    for (uint32_t k = 0; k < objects.size(); ++k) {
-        if (objects[k]->hasEmit()){
-            emit_area_sum += objects[k]->getArea();
-            if (p <= emit_area_sum){
-                objects[k]->Sample(pos, pdf);
-                break;
-            }
-        }
-    }
+void Scene::sampleLight(Intersection& pos, float& pdf) const {
+	float emit_area_sum = 0;
+	for (uint32_t k = 0; k < objects.size(); ++k) {
+		if (objects[k]->hasEmit()) {
+			emit_area_sum += objects[k]->getArea();
+		}
+	}
+	float p = get_random_float() * emit_area_sum;
+	emit_area_sum = 0;
+	for (uint32_t k = 0; k < objects.size(); ++k) {
+		if (objects[k]->hasEmit()) {
+			emit_area_sum += objects[k]->getArea();
+			if (p <= emit_area_sum) {
+				objects[k]->Sample(pos, pdf);
+				break;
+			}
+		}
+	}
 }
 
 bool Scene::trace(
-        const Ray &ray,
-        const std::vector<Object*> &objects,
-        float &tNear, uint32_t &index, Object **hitObject)
-{
-    *hitObject = nullptr;
-    for (uint32_t k = 0; k < objects.size(); ++k) {
-        float tNearK = kInfinity;
-        uint32_t indexK;
-        Vector2f uvK;
-        if (objects[k]->intersect(ray, tNearK, indexK) && tNearK < tNear) {
-            *hitObject = objects[k];
-            tNear = tNearK;
-            index = indexK;
-        }
-    }
+		const Ray& ray,
+		const std::vector<Object*>& objects,
+		float& tNear, uint32_t& index, Object** hitObject) {
+	*hitObject = nullptr;
+	for (uint32_t k = 0; k < objects.size(); ++k) {
+		float tNearK = kInfinity;
+		uint32_t indexK;
+		Vector2f uvK;
+		if (objects[k]->intersect(ray, tNearK, indexK) && tNearK < tNear) {
+			*hitObject = objects[k];
+			tNear = tNearK;
+			index = indexK;
+		}
+	}
 
 
-    return (*hitObject != nullptr);
+	return (*hitObject != nullptr);
 }
 
 // Implementation of Path Tracing
-Vector3f Scene::castRay(const Ray &ray, int depth) const
-{
-    // TO DO Implement Path Tracing Algorithm here
+Vector3f Scene::castRay(const Ray& ray, int depth) const {
+	// TO DO Implement Path Tracing Algorithm here
+
+	// 碰撞判斷
+	Intersection intersToScene = intersect(ray);
+	if (!intersToScene.happened)
+		return Vector3f();
+
+	Vector3f l_dir = {0, 0, 0};		// 直接光照
+	Vector3f l_indir = {0, 0, 0};	// 間接光照
+
+	// 判斷命中材質是否為光源
+	if (intersToScene.m->hasEmission())
+		l_dir += intersToScene.m->getEmission();
+
+	// === 重要性采样 ===
+	Intersection lightPos;
+	float lightPDF = 0.0f;
+	sampleLight(lightPos, lightPDF);
+	Vector3f lightDir = lightPos.coords - intersToScene.coords;
+	float dis = dotProduct(lightDir, lightDir);
+
+	// 與光源之間的碰撞判斷
+	Vector3f lightDir_normalized = lightDir.normalized();
+	Ray rayToLight(intersToScene.coords, lightDir_normalized);
+	Intersection interToLight = intersect(rayToLight);
+	if (interToLight.distance - lightDir.norm() > -0.005) {
+		// 光源能見
+		auto f_r = intersToScene.m->eval(ray.direction, lightDir_normalized, intersToScene.normal);		// BRDF
+		l_dir += lightPos.emit * f_r * dotProduct(lightDir_normalized, intersToScene.normal) * dotProduct(-lightDir_normalized, lightPos.normal) / dis / lightPDF;
+	}
+	// === 重要性采样 ===
+
+	// === 俄羅斯輪盤 ===
+	float RR = 1;
+	if (depth > 5) {
+		if (get_random_float() > RussianRoulette) {
+			return l_dir;
+		}
+		RR = RussianRoulette;
+	}
+	// === 俄羅斯輪盤 ===
+
+	// === 間接光計算 ===
+	Vector3f wi = intersToScene.m->sample(ray.direction, intersToScene.normal).normalized();	// 反射方向
+	Ray indirRay(intersToScene.coords, wi);
+	Intersection intersToPoint = Scene::intersect(indirRay);
+
+	// 相交且不是光源 (間接光計算)
+	if (intersToPoint.happened && !intersToPoint.m->hasEmission()) {
+		float pdf = intersToScene.m->pdf(ray.direction, wi, intersToScene.normal);
+		l_indir = castRay(indirRay, depth + 1) * intersToScene.m->eval(ray.direction, wi, intersToScene.normal) * dotProduct(wi, intersToScene.normal) / RussianRoulette / pdf;
+	}
+	// === 間接光計算 ===
+
+	return l_dir + l_indir;
 }
