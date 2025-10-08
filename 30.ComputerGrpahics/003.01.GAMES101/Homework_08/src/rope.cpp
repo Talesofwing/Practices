07dbe0dc3dd7fcedb53164eeb15570ef25fa626e
zer0 <zerox0103@gmail.com>
2025-10-09 00:23:04 +0900

    [GAMES101] Finished `Homework_08`

--- 30.ComputerGrpahics/003.01.GAMES101/Homework_08/src/rope.cpp
+++ 30.ComputerGrpahics/003.01.GAMES101/Homework_08/src/rope.cpp
@@ -12,11 +12,19 @@ namespace CGL {
     Rope::Rope(Vector2D start, Vector2D end, int num_nodes, float node_mass, float k, vector<int> pinned_nodes)
     {
         // TODO (Part 1): Create a rope starting at `start`, ending at `end`, and containing `num_nodes` nodes.
+        for(int i = 0; i < num_nodes; ++i) {
+            Vector2D pos = start + (end - start) * ((double)i / ((double)num_nodes - 1.0));          
+            masses.push_back(new Mass(pos, node_mass, false));
+        }
+ 
+        for(int i = 0; i < num_nodes - 1; ++i) {
+            springs.push_back(new Spring(masses[i], masses[i + 1], k));
+        }
 
 //        Comment-in this part when you implement the constructor
-//        for (auto &i : pinned_nodes) {
-//            masses[i]->pinned = true;
-//        }
+        for (auto &i : pinned_nodes) {
+            masses[i]->pinned = true;
+        }
     }
 
     void Rope::simulateEuler(float delta_t, Vector2D gravity)
@@ -24,6 +32,9 @@ namespace CGL {
         for (auto &s : springs)
         {
             // TODO (Part 2): Use Hooke's law to calculate the force on a node
+            auto mod_ab = (s->m1->position - s->m2->position).norm();
+            s->m1->forces += -s->k * (s->m1->position - s->m2->position) / mod_ab * (mod_ab - s->rest_length);
+            s->m2->forces += -s->k * (s->m2->position - s->m1->position) / mod_ab * (mod_ab - s->rest_length); 
         }
 
         for (auto &m : masses)
@@ -32,7 +43,25 @@ namespace CGL {
             {
                 // TODO (Part 2): Add the force due to gravity, then compute the new velocity and position
 
+                /*
+                // Explicit Euler method: the next position is calculated using the current velocity.
+                auto a  = m->forces / m->mass + gravity;
+                m->position += m->velocity * delta_t;
+                m->velocity += a * delta_t;
+                */
+
+                /*
+                // Implicit Euler method: the next position is calculated using the next velocity.
+                auto a  = m->forces / m->mass + gravity;
+                m->velocity += a * delta_t;
+                m->position += m->velocity * delta_t;
+                */
+
                 // TODO (Part 2): Add global damping
+                double kd = 0.005;
+                auto a = m->forces / m->mass + gravity - kd * m->velocity / m->mass;
+                m->velocity += a * delta_t;
+                m->position += m->velocity * delta_t;
             }
 
             // Reset all forces on each mass
@@ -45,6 +74,9 @@ namespace CGL {
         for (auto &s : springs)
         {
             // TODO (Part 3): Simulate one timestep of the rope using explicit Verlet （solving constraints)
+            auto mod_ab = (s->m1->position - s->m2->position).norm();
+            s->m1->forces += -s->k * (s->m1->position - s->m2->position) / mod_ab * (mod_ab - s->rest_length);
+            s->m2->forces += -s->k * (s->m2->position - s->m1->position) / mod_ab * (mod_ab - s->rest_length); 
         }
 
         for (auto &m : masses)
@@ -53,9 +85,21 @@ namespace CGL {
             {
                 Vector2D temp_position = m->position;
                 // TODO (Part 3.1): Set the new position of the rope mass
-                
+                /*
+                auto a = m->forces / m->mass + gravity;
+                m->position = temp_position + (temp_position - m->last_position) + a * delta_t * delta_t;
+                m->last_position = temp_position;
+                */
+
                 // TODO (Part 4): Add global Verlet damping
+                auto a = m->forces / m->mass + gravity;
+                double kd = 0.005;
+                m->position = temp_position + (1 - kd) * (temp_position - m->last_position) + a * delta_t * delta_t;
+                m->last_position = temp_position;
             }
+
+            // Reset all forces on each mass
+            m->forces = Vector2D(0, 0);
         }
     }
 }
