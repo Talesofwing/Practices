03e4b7ee7c4ad1d22851f496c621f6f390baa422
zer0 <zerox0103@gmail.com>
2025-10-13 18:51:18 +0900

    [TinyRenderer] Update Lesson0

--- 30.ComputerGrpahics/001.TinyRenderer/TinyRenderer-Lesson0/main.cpp
+++ 30.ComputerGrpahics/001.TinyRenderer/TinyRenderer-Lesson0/main.cpp
@@ -3,10 +3,20 @@
 #include "tgaimage.h"
 
 int main(int argc, char** argv) {
-	std::cout << "===== Lesson 0 =====" << std::endl << std::endl;
+	std::cout << "===== Lesson 0 =====" << std::endl;
 
-	TGAImage image(100, 100, TGAImage::RGB);
-	image.set(52, 41, TGAColor::red);
-	image.write_tga_file("../_results/Lesson0.tga");
+	std::cout << "Start rendering..." << std::endl;
+
+	TGAImage image(64, 64, TGAImage::RGB);
+	int ax = 7, ay = 3;
+	int bx = 12, by = 37;
+	int cx = 62, cy = 53;
+	image.set(ax, ay, TGAColor::white);
+	image.set(bx, by, TGAColor::white);
+	image.set(cx, cy, TGAColor::white);
+	image.write_tga_file("../_results/Lesson0/image.tga");
+
+	std::cout << "Finish rendering..." << std::endl;
+
 	return 0;
 }
