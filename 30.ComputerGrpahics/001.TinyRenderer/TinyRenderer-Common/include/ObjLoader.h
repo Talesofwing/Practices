c1d4371c6e72798df1c748dfa7d2d84100184c8f
zer0 <zerox0103@gmail.com>
2025-10-13 21:40:59 +0900

    [TinyRenderer] Update ObjLoader

--- 30.ComputerGrpahics/001.TinyRenderer/TinyRenderer-Common/include/ObjLoader.h
+++ 30.ComputerGrpahics/001.TinyRenderer/TinyRenderer-Common/include/ObjLoader.h
@@ -25,7 +25,7 @@ public:
 	/// Load .obj file
 	/// </summary>
 	/// <param name="path">The file path</param>
-	/// <param name="fit">Should [-1, 1] be coonverted to [0, 1]</param>
+	/// <param name="fit">Should [-1, 1] be converted to [0, 1]</param>
 	/// <returns>.obj file mesh</returns>
 	static mesh LoadObj(std::string path, bool fit = true);
 };
