71d43ac43e51a8ac3e4d8137ba2aceedf97a5ce6
zer0 <zerox0103@gmail.com>
2026-01-19 19:23:58 +0900

    [RayTracingOneWeekend] Lesson3-Fix paramters

--- 30.ComputerGrpahics/004.RayTracingInOneWeekend/Ray Tracing The Rest of Your Life/main.cpp
+++ 30.ComputerGrpahics/004.RayTracingInOneWeekend/Ray Tracing The Rest of Your Life/main.cpp
@@ -40,8 +40,8 @@ int main() {
 
 	cam.aspect_ratio = 1.0;
 	cam.image_width = 600;
-	cam.samples_per_pixel = 32;
-	cam.max_depth = 15;
+	cam.samples_per_pixel = 64;
+	cam.max_depth = 50;
 	cam.background = color(0, 0, 0);
 
 	cam.vfov = 40;
